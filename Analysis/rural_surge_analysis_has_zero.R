e695b23366d9cb169be7cde7ef527890ea5e33e1
Teresa Wehmeier <88757040+TeresaWehmeier@users.noreply.github.com>
2022-04-10 18:54:57 -0500

    updated R files and an image

--- Analysis/rural_surge_analysis_has_zero.R
+++ Analysis/rural_surge_analysis_has_zero.R
@@ -18,29 +18,29 @@ summary(lm(ur_val ~ avg_0 + avg_1 + avg_2 + avg_3,data=rs_analysis_no_isp))
 rs_no_isp <- read.csv('C:/Users/Teresa/Class/Rural_Surge/Resources/Data/ml_data_has_zero.csv',check.names = F, stringsAsFactors = F)
 
 # - create a total_summary dataframe using the summarize() function to get mean, median, variance, and SD of rs_speed_max max_speed Column
-summary <- rs_no_isp %>% summarize(Mean=mean(tot_0), Median=median(tot_0), Variance=var(tot_0), SD=sd(tot_0), .groups = 'keep')
+summary <- rs_no_isp %>% summarize(Mean=mean(avg_0), Median=median(avg_0), Variance=var(avg_0), SD=sd(avg_0), .groups = 'keep')
 
 
 # create UR_summary dataframe using group_by() and summarize() functions to group by ur_code
-ru_summary_no_isp <- rs_no_isp %>% group_by(urban_rural) %>% summarize(Mean=mean(tot_0), Median=median(tot_0), Variance=var(tot_0), SD=sd(tot_0), .groups = 'keep')
+ru_summary_no_isp <- rs_no_isp %>% group_by(urban_rural) %>% summarize(Mean=mean(avg_0), Median=median(avg_0), Variance=var(avg_0), SD=sd(avg_0), .groups = 'keep')
 
 library(tidyverse)
 
 ## - perform a t-test.Mean set at 100 MB - reference USDA definition of sufficient broadband
-t.test(rs_no_isp$tot_0)
+t.test(rs_no_isp$avg_0)
 
 # perform t-test by ur_code
 rs_no_isp_r <- subset(rs_no_isp, urban_rural=="R")
 rs_no_isp_u <- subset(rs_no_isp, urban_rural=="U")
 
 
-t.test(rs_no_isp_r$tot_0)
-t.test(rs_no_isp_u$tot_0)
+t.test(rs_no_isp_r$avg_0)
+t.test(rs_no_isp_u$avg_0)
 
 ## deeper dive - create box and whisker plot by ur_code and max speed
 ## sample code: p <- ggplot(df, aes(carat, price)) +geom_point() +
 ## labs(title = "Diamonds", x = "x-axis -> Carat", y = "y-axis -> Price")
-ggplot(rs_no_isp, aes(urban_rural, tot_0)) +
+ggplot(rs_no_isp, aes(urban_rural, avg_0)) +
   geom_boxplot() + labs(title = "Population with No ISP by Rural-Urban Classification", x="Rural-Urban",y="Population")
 
 
